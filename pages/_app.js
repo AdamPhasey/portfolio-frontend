8d25b026c28b90d5fe97d7aa2161585ae9007d2c
Adam P <adam.phasey@hotmail.com>
2022-07-09 14:16:28 +0100

    finished auth login

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { UserProvider } from '@auth0/nextjs-auth0';
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
+      <a href="/api/auth/login" className="text-9xl font-bold">Login</a>
       <Component {...pageProps} />
     </UserProvider>
   );
