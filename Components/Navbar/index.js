242a047134d80cbe6f51a26b2e650897a775bd32
Adam P <adam.phasey@hotmail.com>
2022-07-12 18:15:20 +0100

    fixed bug with tabs not showing underscoring correctly

--- Components/Navbar/index.js
+++ Components/Navbar/index.js
@@ -1,16 +1,19 @@
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
+import {useState} from 'react';
+
 
 
 export default function Navbar () {
+const [newValue, setNewValue] = useState();
 
 return (
 
-<Tabs value={0} aria-label="nav tabs example">
-  <Tab className="text-xs md:text-2xl text-white" label="HOME" href="#home" />
-  <Tab className="text-xs md:text-2xl text-white" label="PROJECTS" href="#projects" />
-  <Tab className="text-xs md:text-2xl text-white" label="CONTACT" href="#contact" />
+<Tabs value={newValue} aria-label="nav tabs example">
+  <Tab onClick={() => setNewValue(0)}  className="text-xs md:text-2xl text-white" label="HOME" href="#home" />
+  <Tab onClick={() => setNewValue(1)}  className="text-xs md:text-2xl text-white" label="PROJECTS" href="#projects" />
+  <Tab onClick={() => setNewValue(2)}  className="text-xs md:text-2xl text-white" label="CONTACT" href="#contact" />
 </Tabs>
 
 
