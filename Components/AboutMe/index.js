81fb06e6eeb8963adf929d6c88efa5e0b2f98cff
Adam P <adam.phasey@hotmail.com>
2022-07-12 16:56:53 +0100

    Removed animation of typewriting as was poor quality code

--- Components/AboutMe/index.js
+++ Components/AboutMe/index.js
@@ -6,17 +6,10 @@ function AboutMe() {
     <container className="flex flex-row justify-around items-center bg-[#4a4e69]">
       <Image className="animate-pulse" src="/myComputer.svg" width={750} height={750} />
 
-  <h1 className="mb-2 font-Chakra-Petch text-4xl text-gray-100 md:text-6xl">
-  hi, I&apos;m <br className="block md:hidden" />
-  <span className="relative">
-    <span className="h-20 pt-2 overflow-x-hidden whitespace-nowrap text-brand-accent">
-      vanntile <span className="text-3xl md:text-5xl">👋</span>
-    </span>
-    <span
-      className="{`${styles.cursor} absolute -bottom-0 left-0 -top-1 inline-block bg-gray-900 w-full animate-type will-change`}"
-    ></span>
-  </span>
-</h1>
+  <p className="text-white font-Chakra-Petch">
+
+    Hey, I'm Adam Phasey
+  </p>
     </container>
   );
 }
