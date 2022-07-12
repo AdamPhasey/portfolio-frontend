41222308c7a653aca9b28051b54c45cf878beed9
Adam P <adam.phasey@hotmail.com>
2022-07-12 18:06:13 +0100

    made minor tweaks to config to stop tailwind error

--- Components/Navbar/index.js
+++ Components/Navbar/index.js
@@ -1,14 +1,13 @@
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import Link from "next/link";
-import { useState } from "react";
+
 
 export default function Navbar () {
 
 return (
 
-<Tabs aria-label="nav tabs example">
+<Tabs value={0} aria-label="nav tabs example">
   <Tab className="text-xs md:text-2xl text-white" label="HOME" href="#home" />
   <Tab className="text-xs md:text-2xl text-white" label="PROJECTS" href="#projects" />
   <Tab className="text-xs md:text-2xl text-white" label="CONTACT" href="#contact" />
