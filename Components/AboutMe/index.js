20bce1f75621f63740d3d521f52675acd4c30150
Adam P <adam.phasey@hotmail.com>
2022-07-16 23:50:29 +0100

    fixed typo

--- Components/AboutMe/index.js
+++ Components/AboutMe/index.js
@@ -5,7 +5,7 @@ function AboutMe({value}) {
   return (
     <container className="flex flex-row justify-around items-center bg-[#4841cb] dark:bg-[#4a4e69]">
    {value === 'dark' && <Image className="" alt="a digital creation of a computer" src="/myComputerDarkmode.svg" width={750} height={750} />}
-   {value === 'light' && <Image className="" alt="a digital creation of a computer" src="/myComputerLightmode.svg" width={750} height={750} />}
+   {value === 'light' && <Image className="" alt="a digital creation of a computer" src="/myComputerLightMode.svg" width={750} height={750} />}
 
 
   <p className="text-white font-Chakra-Petch">
