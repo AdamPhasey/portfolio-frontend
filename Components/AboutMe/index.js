8c84be41a5866a96b0a4088bcf333c4cc6441ded
Adam P <adam.phasey@hotmail.com>
2022-07-22 00:20:57 +0100

    Added responsive text font using vmin to text in header and changed colour palette to blend heaader into body

--- Components/AboutMe/index.js
+++ Components/AboutMe/index.js
@@ -1,32 +1,41 @@
 import Image from "next/image";
 import { Container } from "postcss";
+import {useEffect} from 'react';
+import anime from 'animejs';
 
 function AboutMe({ value }) {
+//   useEffect(() => {
+//   let comp = document.querySelector('#Comp1')
+ 
+// }, [])
+
   return (
-    <container className="flex h-full w-full flex-row content-center items-center bg-[#faedcd] dark:bg-[#4a4e69]">
-      <div className="hidden p-10 md:flex justify-center md:w-1/2">
+    <container className="flex h-full w-full flex-row content-center items-center bg-[#f4f3ee] dark:bg-[#4a4e69]">
+      <div  className="hidden p-10 md:flex justify-center md:h-screen md:w-1/2">
         {value === "dark" && (
           <Image
+            id="Comp1"
             className="invisible md:visible"
             alt="a digital creation of a computer"
             src="/myComputerDarkmode.svg"
-            width={500}
-            height={500}
+            width={650}
+            height={650}
           />
         )}
         {value === "light" && (
           <Image
+            id="Comp1"
             className="hidden md:flex justify-center md:w-1/2"
             alt="a digital creation of a computer"
             src="/myComputerLightMode.svg"
-            width={500}
-            height={500}
+            width={650}
+            height={650}
           />
         )}
       </div>
 
-      <div className="text-blue-400 text-[9vw] py-40 p-4 font-Montserrat-Alternates md:text-[3vw] md:w-1/2 md:relative md:p-0">
-        <p className="text-green-400 text-[4vw] md:text-[2vw]">
+      <div className="text-blue-400 text-[9vmin] py-40 p-4 font-Montserrat-Alternates md:text-[6vmin] md:w-1/2 md:relative md:p-0">
+        <p className="text-green-400 text-[4vmin] md:text-[4vmin]">
           👋 Hi, I'm Adam!
         </p>
         <h1 >
--- Components/Projects/index.js
+++ Components/Projects/index.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Cards from "../Cards";
+import anime from "animejs";
+import { useEffect, useState } from "react" ;
 
 function Projects() {
+
   return (
     <>
      <Cards />
