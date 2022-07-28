ab8927695290fd6ea8f7d4751af9fd595b794867
Adam P <adam.phasey@hotmail.com>
2022-07-29 00:31:31 +0100

    Changed svg computer to have a border. Added text align to about me text. Started work on cards. Updated dragging components to useRef hook instead of querySelector

--- Components/AboutMe/index.js
+++ Components/AboutMe/index.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { Container } from "postcss";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import anime from "animejs";
 
 function AboutMe({ value }) {
 
-
+const ref = useRef(null);
 //   use effect here if needed
   useEffect(() => {
-let el = document.querySelector("#terminal")
+let el = ref.current
 let xPos = 0;
 let yPos = 0;
 if (el.querySelector('header'))
@@ -36,48 +36,48 @@ function closeDragElement() {
 
 
   return (
-    <container className="flex h-full w-full flex-row content-center items-center bg-[#f4f3ee] dark:bg-[#4a4e69]">
-      <div id="terminal" className="hidden z-50 cursor-move sm:p-10 lg:p-24 md:flex justify-center md:h-screen md:w-1/2">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" >
+    <container className="flex text-center h-full w-full flex-row content-center items-center bg-[#4a4e69] dark:bg-[#000]">
+      <div ref={ref} className="hidden z-50 cursor-move sm:p-10 md:p-10 lg:p-28 md:flex justify-center md:h-screen md:w-1/2">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
     <defs>
-        <style>.cls-1{"{fill:gray;}.cls-2{fill:#d90028;}.cls-3{fill:#ffc10b;}.cls-4{fill:#00cf00;}.cls-5{font-size:35px;fill:#f2f2f2;}.cls-5,.cls-6{font-family:Silom;}.cls-6{font-size:50px;fill:lime;{"}</style>
+        <style>.cls-1{"{stroke:gray;stroke-miterlimit:10;stroke-width:5px;}.cls-2{fill:gray;}.cls-3{fill:#d90028;}.cls-4{fill:#ffc10b;}.cls-5{fill:#00cf00;}.cls-6{font-size:17.43px;fill:#f2f2f2;}.cls-6,.cls-7{font-family:Silom;}.cls-7{font-size:24.9px;fill:lime;{"}</style>
     </defs>
     <g id="Background">
-        <rect width="1000" height="1000" />
+        <rect className="cls-1" x="2.5" y="2.5" width="495" height="495" />
     </g>
     <g id="Nav">
-        <rect className="cls-1" width="1000" height="57" />
-        <circle className="cls-2" cx="39" cy="28" r="16" />
-        <circle className="cls-3" cx="87.41" cy="28" r="16" />
-        <circle className="cls-4" cx="135" cy="28" r="16" />
-        <circle cx="39.41" cy="28.41" r="4.92" />
-        <text className="cls-5" transform="translate(301.7 44.36)">Adam Phasey’s Stack</text>
+        <rect className="cls-2" x="2" y="1" width="498" height="28.39" />
+        <circle className="cls-3" cx="21.42" cy="14.94" r="7.97" />
+        <circle className="cls-4" cx="45.53" cy="14.94" r="7.97" />
+        <circle className="cls-5" cx="69.23" cy="14.94" r="7.97" />
+        <circle cx="21.63" cy="15.15" r="2.45" />
+        <text className="cls-6" transform="translate(152.25 23.09)">Adam Phasey’s Stack</text>
     </g>
     <path id="path">
         <animate attributeName="d" from="m0,0 h0" to="m0,0 h1100" dur="2s" begin="0s" repeatCount="0" fill="freeze"/>
     </path>
     <g id="Text">
-        <text className="cls-6" transform="translate(59.4 153.18)"><textPath xlinkHref="#path">JavaScript</textPath></text>
-        <text className="cls-6" transform="translate(59.49 227.87)"><textPath xlinkHref="#path">TypeScript</textPath></text>
-        <text className="cls-6" transform="translate(61.49 377.87)"><textPath xlinkHref="#path">HTML &amp; CSS</textPath></text>
-        <text className="cls-6" transform="translate(61.49 302.87)"><textPath xlinkHref="#path">Node.js</textPath></text>
-        <text className="cls-6" transform="translate(64 447.87)"><textPath xlinkHref="#path">Python</textPath></text>
-        <text className="cls-6" transform="translate(62.49 517.87)"><textPath xlinkHref="#path">Express</textPath></text>
-        <text className="cls-6" transform="translate(64.49 586.87)"><textPath xlinkHref="#path">React</textPath></text>
-        <text className="cls-6" transform="translate(64.49 651.87)"><textPath xlinkHref="#path">AWS</textPath></text>
-        <text className="cls-6" transform="translate(64.49 713.5)"><textPath xlinkHref="#path">Deployment</textPath></text>
-        <text className="cls-6" transform="translate(66.49 783.5)"><textPath xlinkHref="#path">PostgreSQL/MySQL</textPath></text>
-        <text className="cls-6" transform="translate(61.49 937.87)"><textPath xlinkHref="#path">&gt;_</textPath></text>
+        <text className="cls-7" transform="translate(31.58 77.28)"><textPath xlinkHref="#path">JavaScript</textPath></text>
+        <text className="cls-7" transform="translate(31.63 114.48)"><textPath xlinkHref="#path">TypeScript</textPath></text>
+        <text className="cls-7" transform="translate(32.62 189.18)"><textPath xlinkHref="#path">HTML &amp; CSS</textPath></text>
+        <text className="cls-7" transform="translate(32.62 151.83)"><textPath xlinkHref="#path">Node.js</textPath></text>
+        <text className="cls-7" transform="translate(33.87 224.04)"><textPath xlinkHref="#path">Python</textPath></text>
+        <text className="cls-7" transform="translate(33.12 258.9)"><textPath xlinkHref="#path">Express</textPath></text>
+        <text className="cls-7" transform="translate(34.12 293.26)"><textPath xlinkHref="#path">React</textPath></text>
+        <text className="cls-7" transform="translate(34.12 325.63)"><textPath xlinkHref="#path">AWS</textPath></text>
+        <text className="cls-7" transform="translate(34.12 356.32)"><textPath xlinkHref="#path">Deployment</textPath></text>
+        <text className="cls-7" transform="translate(35.11 391.18)"><textPath xlinkHref="#path">PostgreSQL/MySQL</textPath></text>
+        <text className="cls-7" transform="translate(32.62 468.06)"><textPath xlinkHref="#path">&gt;_</textPath></text>
     </g>
 </svg>
 </div>
 
-      <div className="text-blue-400 text-[9vmin] py-40 p-4 font-Montserrat-Alternates md:text-[6vmin] md:w-1/2 md:relative md:p-0">
+      <div className="text-white text-[9vmin] py-40 p-4 font-Montserrat-Alternates md:text-[5vmin] md:w-1/2 md:relative md:p-0">
         <p className="text-green-400 text-[4vmin] md:text-[4vmin]">
           👋 Hi, I am Adam!
         </p>
         <h1>
-          I used to be in law and business. Now I make <a className="text-green-400">fun</a> and <a className="text-green-400">interactive</a> things with <a className="text-green-400">code</a>.
+          I am a London based full stack developer. I enjoy making <a className="text-green-400">fun</a> and <a className="text-green-400">interactive</a> things with <a className="text-green-400">code</a>.
         </h1>
       </div>
     </container>
--- Components/Cards/index.js
+++ Components/Cards/index.js
@@ -3,18 +3,45 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import {useEffect, useRef} from 'react'
 
 export default function Cards() {
+const ref = useRef(null);
+
+  useEffect(() => {
+let el = ref.current
+    console.log(el)
+    let xPos = 0;
+    let yPos = 0;
+    el.addEventListener('mousedown', dragMouseDown);
+    function dragMouseDown(event) {
+      event.preventDefault();
+      document.addEventListener('mouseup', closeDragElement);
+      document.addEventListener('mousemove', elementDrag);
+    }
+    function elementDrag(event) {
+      event.preventDefault();
+      xPos += event.movementX;
+      yPos += event.movementY;
+      el.style.transform = `translate(${xPos}px, ${yPos}px)`;
+    }
+    function closeDragElement() {
+      document.removeEventListener('mouseup', closeDragElement);
+      document.removeEventListener('mousemove', elementDrag);
+    }
+      }, []);
+
+
   return (
     <>
-      <Card sx={{ maxWidth: 345 }}>
+      <Card ref={ref} sx={{ maxWidth: 400, maxHeight: 400 }}>
         <CardActionArea>
-          <CardMedia component="img" height="140" image="" alt="green iguana" />
+        <CardMedia component="img" className="p-10" image="/npm.svg" alt="npm package" />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               SPACE COIN
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography className="text-sm" color="text.secondary">
               Lizards are a widespread group of squamate reptiles, with over
               6,000 species, ranging across all continents except Antarctica
             </Typography>
@@ -30,14 +57,37 @@ export default function Cards() {
         </CardActions>
       </Card>
 
-      <Card sx={{ maxWidth: 345 }}>
+      <Card ref={ref} sx={{ maxWidth: 400, maxHeight: 400 }}>
         <CardActionArea>
-          <CardMedia component="img" height="140" image="" alt="green iguana" />
+          <CardMedia component="img" className="p-10" image="/npm.svg" alt="npm package" />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              INFINITESIMAL
+            </Typography>
+            <Typography className="text-sm" color="text.secondary">
+              I made my first npmjs package which serves to help developers to remove blank spaces between strings.
+              This is particularly useful with database submissions. In my first week of publication I had over 300 downloads!
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Github
+          </Button>
+          <Button size="small" color="primary">
+            Live
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card ref={ref} sx={{ maxWidth: 400, maxHeight: 400 }}>
+        <CardActionArea>
+        <CardMedia component="img" className="p-10" image="/npm.svg" alt="npm package" />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               BACKPACK
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography className="text-sm" color="text.secondary">
               Lizards are a widespread group of squamate reptiles, with over
               6,000 species, ranging across all continents except Antarctica
             </Typography>
@@ -53,37 +103,14 @@ export default function Cards() {
         </CardActions>
       </Card>
 
-      <Card sx={{ maxWidth: 345 }}>
+      <Card ref={ref} sx={{ maxWidth: 400, maxHeight: 400 }}>
         <CardActionArea>
-          <CardMedia component="img" height="140" image="" alt="green iguana" />
+        <CardMedia component="img" className="p-10" image="/npm.svg" alt="npm package" />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               BACKPACK
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Github
-          </Button>
-          <Button size="small" color="primary">
-            Live
-          </Button>
-        </CardActions>
-      </Card>
-
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia component="img" height="140" image="" alt="green iguana" />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              BACKPACK
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography className="text-sm" color="text.secondary">
               Lizards are a widespread group of squamate reptiles, with over
               6,000 species, ranging across all continents except Antarctica
             </Typography>
