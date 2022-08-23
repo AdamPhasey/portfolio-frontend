83b30f330cdb50199af1b650c31c32ffb2aeae95
Adam P <adam.phasey@hotmail.com>
2022-08-23 18:48:43 +0100

    Added group youtube video

--- pages/chews.js
+++ pages/chews.js
@@ -14,13 +14,24 @@ export default function Chews() {
           Chews - Recipe Directory App
         </h1>
       </div>
+      <h1 className="flex flex-row justify-center text-white text-4xl mb-10">Our group presentation</h1>
+      <div className="flex flex-row justify-center mb-10">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/aXnJCW1pesY"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
       <div className="flex flex-row justify-center">
         <MuiCarousel
           images={[
             {
               label: "Our wireframes",
-              imgPath:
-                "/chews-wireframes.png",
+              imgPath: "/chews-wireframes.png",
             },
             {
               label: "Bird",
