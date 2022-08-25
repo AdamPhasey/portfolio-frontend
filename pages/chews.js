a41d2540db145d172144c873fca154ca74d4b63c
Adam P <adam.phasey@hotmail.com>
2022-08-26 00:30:22 +0100

    added pics to chews subpage slideshow

--- pages/chews.js
+++ pages/chews.js
@@ -52,14 +52,24 @@ export default function Chews() {
               imgPath: "/chews-wireframes.png",
             },
             {
+              label: "Manifesto",
+              imgPath:
+                "/manifesto.png",
+            },
+            {
               label: "Disney ideation",
               imgPath:
-                "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+                "/disney-chews.png",
             },
             {
               label: "Interface",
               imgPath:
-                "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+                "/chews-user-story.png",
+            },
+            {
+              label: "Questionnaire Responses",
+              imgPath:
+                "/chews-research.png",
             },
           ]}
         />
